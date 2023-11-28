aa25417b4d284b2a002182836214bfcca0596bcb
Pedro Hurtado <pedrohurt@gmail.com>
2023-11-28 12:18:35 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import Array from "./Array";import Login  from "./Login";
+//import Parent from "./parent/parent";
+
+import Calendar from "./calendar/calendar";
+import Counter from "./counter/counter";
+
+function App() {
+  //const foo = {name:'pedro'}
+  return (
+    <>      
+      <Counter/>
+      <Calendar/>      
+    </>
+    
+  );
+}
+
+export default App;
